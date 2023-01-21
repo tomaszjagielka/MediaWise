63842891eb9aa0bcfe82f2001a5e5b0fd10c4a25
Tomasz Jagiełka <tomasz.m.jagielka@gmail.com>
2023-01-21 16:16:27 +0100

    Add proof of concept to the search window

--- extension/popup/search-popup.js
+++ extension/popup/search-popup.js
@@ -1,35 +1,10 @@
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-let paramString = window.location.href.split('?')[1];
-let queryString = new URLSearchParams(paramString);
+const paramString = window.location.href.split('?')[1];
+const queryString = new URLSearchParams(paramString);
 
 let links = [];
 let titles = [];
 let descriptions = [];
+let serviceNames = [];
 
 for (let pair of queryString.entries()) {
     if (pair[0] === "links") {
@@ -38,15 +13,40 @@ for (let pair of queryString.entries()) {
         titles = pair[1].split("|-|,");
     } else if (pair[0] === "descriptions") {
         descriptions = pair[1].split("|-|,");
+    } else if (pair[0] === "serviceNames") {
+        serviceNames = pair[1].split("|-|,");
     }
-
-   console.log("Key is: " + pair[0]);
-   console.log("Value is: " + pair[1]);
 }
 
 console.log(links);
 console.log(titles);
 console.log(descriptions);
+console.log(serviceNames);
+
+const length = Math.max(links.length, titles.length, descriptions.length, serviceNames.length);
+console.log(length);
+
+for (let i = 0; i < length; i++) {
+    const item = document.createElement("div");
+    item.className = "item";
+
+    const serviceName = document.createElement("h2");
+    if (i < length) // Safety procaution.
+        serviceName.textContent = serviceNames[i];
+
+    const description = document.createElement("p");
+    if (i < length)
+        description.textContent = descriptions[i];
+
+    const separator = document.createElement("hr");
+
+    item.appendChild(serviceName);
+    item.appendChild(description);
+    item.appendChild(separator);
+
+    document.body.appendChild(item);
+}
+
 
 // var search = location.search.substring(1);
 // const search2 = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -15,58 +15,68 @@ function checkAuthorBackground(info, tab) {
     // 127.0.0.1 is the address of the server.
     // Direct request, such as https://customsearch.googleapis.com/customsearch/v1?q=Ed%20Krassenstein&cx=f393ab8cb82b8474f&key=GOOGLE_SEARCH_API_KEY
     // also works, but it exposes the API key. A safer approach would be to send the request on the server.
-    // fetch('127.0.0.1').then(r => r.text()).then(result => {
-    //     // Result now contains the response text, do what you want...
-    //     console.log(result);
-    // })
+    fetch('https://google.com/').then(r => r.text()).then(result => {
+        // Result now contains the response text, do what you want...
+        console.log(result);
 
-    const requestData = {
-        "links":
-            ["https://en.wikipedia.org/wiki/Brian_and_Ed_Krassenstein|-|",
-            "https://mobile.twitter.com/edkrassen|-|",
-            "https://www.instagram.com/ed_krassenstein/?hl=en|-|",
-            "https://www.rollingstone.com/culture/culture-news/krassenstein-brothers-twitter-elon-musk-1234646852/|-|",
-            "https://knowyourmeme.com/memes/people/brian-and-ed-krassenstein"],
-        "titles": 
-            ["Brian and Ed Krassenstein - Wikipedia|-|",
-            "Ed Krassenstein (@EdKrassen) / Twitter|-|",
-            "Eddie Krassenstein (@ed_krassenstein) • Instagram photos|-|",
-            "Krassenstein Brothers Return to Twitter to Battle Elon Musk|-|",
-            "Brian and Ed Krassenstein | Know Your Meme"],
-        "descriptions":
-            ["Brian and Ed Krassenstein are American twin brothers who are writers, entrepreneurs, and social media personalities. They reside in Fort Myers, Florida and ...|-|",
-            "Ed Krassenstein. @EdKrassen. Living in reality. Follow my twin: @Krassenstein . Web3 futurist. DeSo: http://diamondapp.com/u/Krassenstein Subscribe on ...|-|",
-            "Eddie Krassenstein. Building on the DeSo Blockchain. Co-founded 3dprint.com, NFTz.me, HillReporter.com, etc. diamondapp.com/u/Krassenstein. 51 posts.|-|",
-            "Twin brothers Edward and Brian Krassenstein, of Fort Myers, Florida, were once little-known entrepreneurs who ran investment web forums that ...|-|",
-            "Brian and Ed Krassenstein are American twin brothers known for their presence on Twitter as part of the anti-Trump Resistance movement."]
-    }
+        const requestData = {
+            // "links":
+            //     ["https://en.wikipedia.org/wiki/Brian_and_Ed_Krassenstein|-|",
+            //     "https://mobile.twitter.com/edkrassen|-|",
+            //     "https://www.instagram.com/ed_krassenstein/?hl=en|-|",
+            //     "https://www.rollingstone.com/culture/culture-news/krassenstein-brothers-twitter-elon-musk-1234646852/|-|",
+            //     "https://knowyourmeme.com/memes/people/brian-and-ed-krassenstein"],
+            // "titles": 
+            //     ["Brian and Ed Krassenstein - Wikipedia|-|",
+            //     "Ed Krassenstein (@EdKrassen) / Twitter|-|",
+            //     "Eddie Krassenstein (@ed_krassenstein) • Instagram photos|-|",
+            //     "Krassenstein Brothers Return to Twitter to Battle Elon Musk|-|",
+            //     "Brian and Ed Krassenstein | Know Your Meme"],
+            // "descriptions":
+            // ["Brian and Ed Krassenstein are American twin brothers who are writers, entrepreneurs, and social media personalities. They reside in Fort Myers, Florida and ...|-|",
+            // "Ed Krassenstein. @EdKrassen. Living in reality. Follow my twin: @Krassenstein . Web3 futurist. DeSo: http://diamondapp.com/u/Krassenstein Subscribe on ...|-|",
+            // "Eddie Krassenstein. Building on the DeSo Blockchain. Co-founded 3dprint.com, NFTz.me, HillReporter.com, etc. diamondapp.com/u/Krassenstein. 51 posts.|-|",
+            // "Twin brothers Edward and Brian Krassenstein, of Fort Myers, Florida, were once little-known entrepreneurs who ran investment web forums that ...|-|",
+            // "Brian and Ed Krassenstein are American twin brothers known for their presence on Twitter as part of the anti-Trump Resistance movement."]
+    
+            "serviceNames": 
+                ["Wikipedia|-|", "Lorem ipsum|-|", "Lorem ipsum|-|", "Lorem ipsum|-|", "Lorem ipsum"],
+            "descriptions":
+                ["Barack Hussein Obama II (/bəˈrɑːk huːˈseɪn oʊˈbɑːmə/ (listen) bə-RAHK hoo-SAYN oh-BAH-mə;[1] born August 4, 1961) is an American politician who served as the 44th president of the United States from 2009 to 2017. A member of the Democratic Party, Obama was the first African-American president of the United States.[2] He previously served as a U.S. senator from Illinois from 2005 to 2008 and as an Illinois state senator from 1997 to 2004, and previously worked as a civil rights lawyer before entering politics.|-|",
+                "Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.|-|",
+                "Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.|-|",
+                "Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus.|-|",
+                "Pellentesque nec elit vitae mauris viverra commodo. Nulla luctus leo a diam tempus, sed viverra lectus maximus."]
+        }
 
-    // console.log(requestData);
+        console.log(requestData);
 
-    // chrome.scripting.executeScript({
-    //     files: ["content.js"],
-    //     target: {tabId: tab.id}
-    // })
-
-    // chrome.windows.create({'url': 'popup/search-popup.html?test=5', 'type': 'popup'}, function(window) {
-    // });
-
-    const requestDataParams = new URLSearchParams(requestData).toString();
-
-    chrome.windows.create({url: "popup/search-popup.html?" + requestDataParams, type: "popup"});
-
-    // (async () => {
-    //     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    //     const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-    //     // do something with response here, not outside the function
-    //     console.log(response);
-    // })();
+        // chrome.scripting.executeScript({
+        //     files: ["content.js"],
+        //     target: {tabId: tab.id}
+        // })
+    
+        const requestDataParams = new URLSearchParams(requestData).toString();
+    
+        chrome.windows.create({url: "popup/search-popup.html?" + requestDataParams, type: "popup"});
+    
+        // (async () => {
+        //     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        //     const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+        //     // do something with response here, not outside the function
+        //     console.log(response);
+        // })();
+    })
 }
 
-chrome.contextMenus.create({
-    title: "Search: %s", 
-    contexts: ["selection"], 
-    id: CONTEXT_MENU_ID
-});
+if (chrome.contextMenus) {
+    chrome.contextMenus.removeAll(function() {
+        chrome.contextMenus.create({
+            title: "Search: %s", 
+            contexts: ["selection"], 
+            id: CONTEXT_MENU_ID
+        });
+    });
 
-chrome.contextMenus.onClicked.addListener(checkAuthorBackground);
+    chrome.contextMenus.onClicked.addListener(checkAuthorBackground);
+}
